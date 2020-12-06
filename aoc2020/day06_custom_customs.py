df472d8d2ea337d7285d0a18041ed25deef5fcbd
codesammy <codesammy@example.org>
2020-12-06 11:12:55 +0100

    day 6 part 1

---
+++ aoc2020/day06_custom_customs.py
@@ -0,0 +1,54 @@
+import sys
+from functools import reduce
+sys.path.append('../')
+from util import read_file, assert_equals
+
+class Person:
+    def __init__(self, line):
+        self.answers = set((x for x in line))
+
+class Group:
+    def __init__(self):
+        self.persons = []
+        
+    def add(self, person):
+        self.persons.append(person)
+
+    def anyone_yes(self):
+        return reduce(set.union, (p.answers for p in self.persons))
+    
+def parse_groups(lines):
+    groups = []
+    g = Group()
+    groups.append(g)
+    for line in lines:
+        if line == "":
+            g = Group()
+            groups.append(g)
+        else:
+            g.add(Person(line))
+    return groups
+
+def part1(lines):
+    groups = parse_groups(lines)
+    return sum(map(len, map(Group.anyone_yes, groups)))
+
+sample = """abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b""".split("\n")
+assert_equals(part1(sample), 11)
+
+lines = read_file(sys.argv[0].replace("py", "input"))
+print(part1(lines))
