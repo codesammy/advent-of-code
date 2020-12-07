ab5f4185c8cefe2d388e620a805a48b9d7fb2595
codesammy <codesammy@example.org>
2020-12-07 20:39:05 +0100

    day 7 part 2

--- aoc2020/day07_handy_haversacks.py
+++ aoc2020/day07_handy_haversacks.py
@@ -23,7 +23,7 @@ class BagRuleParser:
             for count, other_color in self.bag_pattern.findall(m.group(2)):
                 if other_color not in self.rules:
                     self.rules[other_color] = BagRule(other_color)
-                self.rules[color].children.add(self.rules[other_color])
+                self.rules[color].children.append((int(count),self.rules[other_color]))
                 self.rules[other_color].parents.add(self.rules[color])
         else:
             raise Exception(f"unknown rule: {line}")
@@ -35,12 +35,21 @@ class BagRule:
     def __init__(self, color):
         self.color = color
         self.parents = set()
-        self.children = set()
+        self.children = list()
 
     def accept_up(self, visitor):
         for p in self.parents:
             visitor.visit(p)
             p.accept_up(visitor)
+
+    def calculate_deep_children_bag_count(self):
+        if len(self.children) == 0:
+            return 1
+        else:
+            total = 0
+            for count, child in self.children:
+                total += count * child.calculate_deep_children_bag_count()
+            return 1 + total
 
     def __repr__(self):
         return self.__str__()
@@ -48,20 +57,26 @@ class BagRule:
     def __str__(self):
         return f'BagRule("{self.color}",parents={len(self.parents)},children={len(self.children)})'
 
-class BagRuleParentVisitor:
+class BagRuleParentColorCountVisitor:
     def __init__(self):
         self.colors = set()
 
     def visit(self, bag_rule):
         self.colors.add(bag_rule.color)
-    
+        
 def part1(lines, my_bag_color):
     rules = BagRuleParser()
     for l in lines:
         rules.parse_line(l)
-    parent_visitor = BagRuleParentVisitor()
-    rules.rules[my_bag_color].accept_up(parent_visitor)
-    return len(parent_visitor.colors)
+    visitor = BagRuleParentColorCountVisitor()
+    rules.rules[my_bag_color].accept_up(visitor)
+    return len(visitor.colors)
+
+def part2(lines, my_bag_color):
+    rules = BagRuleParser()
+    for l in lines:
+        rules.parse_line(l)
+    return rules.rules[my_bag_color].calculate_deep_children_bag_count()-1
 
 sample = """light red bags contain 1 bright white bag, 2 muted yellow bags.
 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
@@ -73,6 +88,8 @@ vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags.""".split("\n")
 assert_equals(part1(sample, "shiny gold"), 4)
+assert_equals(part2(sample, "shiny gold"), 32)
 
 lines = read_file(sys.argv[0].replace("py", "input"))
 print(part1(lines, "shiny gold"))
+print(part2(lines, "shiny gold"))
