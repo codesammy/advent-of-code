35beb7217f69bcf086ce04cd88aff4ccdff7ebb1
codesammy <codesammy@example.org>
2020-12-21 11:53:01 +0100

    day 21 part 2

--- aoc2020/day21_allergen_assessment.py
+++ aoc2020/day21_allergen_assessment.py
@@ -14,10 +14,10 @@ class Food:
     ingredients: Set[str] = field(compare=False)
     allergens: Set[str] = field(compare=False)
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, order=True)
 class Allergen:
     name: str
-    ingredient: str
+    ingredient: str = field(compare=False)
 
 def parse_input(lines):
     food_pattern = re.compile(r"(\w+(?: \w+)*)+ \(contains (\w+(?:, \w+)*)\)")
@@ -25,7 +25,7 @@ def parse_input(lines):
         ingredients, allergens = food_pattern.fullmatch(line).groups()
         yield Food(ingredients, set(ingredients.split(" ")), set(allergens.split(", ")))
 
-def part1(lines):
+def find_allergens(lines):
     foods = list(parse_input(lines))
 
     # sort by allergen
@@ -52,15 +52,25 @@ def part1(lines):
                 for other_ingredients in allergen_ingredient_candidates.values():
                     if ingredient in other_ingredients:
                         other_ingredients.discard(ingredient)
+    return allergens, foods
+
+def part1(lines):
+    allergens, foods = find_allergens(lines)
 
     # count non allergens
     return sum(len(food.ingredients.difference({a.ingredient for a in allergens})) for food in foods)
+
+def part2(lines):
+    allergens, _ = find_allergens(lines)
+    return ",".join(map(lambda a: a.ingredient, sorted(allergens)))
 
 sample1 = """mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
 trh fvjkl sbzzf mxmxvkd (contains dairy)
 sqjhc fvjkl (contains soy)
 sqjhc mxmxvkd sbzzf (contains fish)""".split("\n")
 assert_equals(part1(sample1), 5)
+assert_equals(part2(sample1), "mxmxvkd,sqjhc,fvjkl")
 
 lines = read_file(sys.argv[0].replace("py", "input"))
 print(part1(lines))
+print(part2(lines))
