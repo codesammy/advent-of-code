2a611d74085cb207b9cb6d832f6c8335eacd0af5
codesammy <codesammy@example.org>
2020-12-22 18:09:21 +0100

    adds read_file_one_string function

--- util.py
+++ util.py
@@ -4,6 +4,12 @@ def read_file(filename="input.txt"):
     inputfile.close()
     return [x.strip() for x in lines]
 
+def read_file_one_string(filename="input.txt"):
+    inputfile = open(filename, "r")
+    res = inputfile.read()
+    inputfile.close()
+    return res
+
 def assert_equals(actual, expected):
     if (actual != expected):
         raise(Exception("%s was not %s" % (actual, expected)))
