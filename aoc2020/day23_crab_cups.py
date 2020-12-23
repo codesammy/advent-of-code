ae52862be0f22747c1293bf844697ff68a913c32
codesammy <codesammy@example.org>
2020-12-23 14:21:28 +0100

    day 23 part 2

--- aoc2020/day23_crab_cups.py
+++ aoc2020/day23_crab_cups.py
@@ -3,71 +3,95 @@ sys.path.append('../')
 from util import read_file_one_string, assert_equals
 
 from dataclasses import dataclass, field
-from typing import List
+from typing import Dict, List
 from itertools import cycle, islice
+import math
 
 @dataclass
 class Cup:
     label: str
-
-    def value(self):
-        return int(self.label)
+    value: int
+    next_cup: type = field(init=False)
 
     def __str__(self):
         return self.label
 
     def __lt__(self, other):
-        return self.label < other.label
+        return self.value < other.value
 
 @dataclass
 class Game:
     cups: List[Cup]
+    max_value: int
     current_cup: Cup = field(init=False)
     move_count: int = field(default=1)
+    one: Cup = field(init=False)
+    lookup_by_value: Dict[int, Cup] = field(init=False, default_factory=dict)
+    debug: bool = field(init=False, default=False)
 
     def __post_init__(self):
         self.current_cup = self.cups[0]
+        self.one_cup = next(c for c in self.cups[:9] if c.value == 1)
+        # link cups in circle
+        last_cup = self.cups[-1]
+        for cup in self.cups:
+            last_cup.next_cup = cup
+            last_cup = cup
+            self.lookup_by_value[cup.value] = cup
 
     def move(self):
-        print(f"-- move {self.move_count} --")
-        print(f"cups: {self}")
         picked_up_cups = self.pick_up()
-        print(f"pick up: {self.__str__cups(picked_up_cups)}")
-        destination_cup = self.select_destination()
-        print(f"destination: {destination_cup.label}")
+        destination_cup = self.select_destination(picked_up_cups)
         self.place_cups(destination_cup, picked_up_cups)
+        if self.debug:
+            print(f"-- move {self.move_count} --")
+            print(f"cups: {self}")
+            print(f"pick up: {self.__str__cups(picked_up_cups)}")
+            print(f"destination: {destination_cup.label}")
         self.select_new_current_cup()
         self.move_count += 1
 
     def pick_up(self, amount=3):
-        current_cup_index = self.cups.index(self.current_cup)
-        lookup_cups = self.cups*3
-        picked_up_cups = lookup_cups[current_cup_index + 1:current_cup_index + 1 + amount]
-        for picked in picked_up_cups:
-            self.cups.remove(picked)
+        picked_up_cups = [self.current_cup.next_cup]
+        picked_up_cups += [picked_up_cups[-1].next_cup]
+        picked_up_cups += [picked_up_cups[-1].next_cup]
+
+        self.current_cup.next_cup = picked_up_cups[-1].next_cup
         return picked_up_cups
 
-    def select_destination(self):
-        lookup_cups = iter(cycle(reversed(sorted(self.cups))))
-        while next(lookup_cups) != self.current_cup:
-            pass
-        return next(lookup_cups)
+    def select_destination(self, picked_up_cups):
+        destination = None
+        start_value = self.current_cup.value - 1
+        picked_up_values = list(map(lambda c: c.value, picked_up_cups))
+        local_max_value = max(set(range(self.max_value-3,self.max_value+1))-set(picked_up_values))
+        while destination == None:
+            if start_value == 0:
+                start_value = local_max_value
+            if start_value not in picked_up_values:
+                destination = start_value
+            start_value -= 1
+        return self.lookup_by_value[destination]
 
     def place_cups(self, destination_cup, picked_up_cups):
-        destination_cup_index = self.cups.index(destination_cup)
-        self.cups = self.cups[:destination_cup_index+1] + picked_up_cups + self.cups[destination_cup_index+1:]
+        temp = destination_cup.next_cup
+        destination_cup.next_cup = picked_up_cups[0]
+        picked_up_cups[-1].next_cup = temp
 
     def select_new_current_cup(self):
-        current_cup_index = self.cups.index(self.current_cup)
-        lookup_cups = self.cups*2
-        self.current_cup = lookup_cups[current_cup_index + 1]
+        self.current_cup = self.current_cup.next_cup
 
     def order(self):
-        cups = iter(cycle(self.cups))
-        while next(cups).value() != 1:
-            pass
-        cups_after_one = islice(cups, 9-1)
-        return "".join(map(str, cups_after_one))
+        cup = self.one_cup
+        order_cups = []
+        for _ in range(len(self.cups) - 1):
+            cup = cup.next_cup
+            order_cups.append(cup)
+        return "".join(map(str, order_cups))
+
+    def product(self):
+        a = self.one_cup.next_cup
+        b = a.next_cup
+        return a.value * b.value
 
     def __str__(self):
         return self.__str__cups(self.cups)
@@ -78,21 +102,37 @@ class Game:
                 return '(' + str(cup) + ')'
             else:
                 return str(cup)
-        return " ".join(map(parens_around_current_cup, cups))
+        starting_cup = cups[0]
+        cup = starting_cup
+        str_cups = [cup]
+        for _ in range(len(cups) - 1):
+            cup = cup.next_cup
+            str_cups.append(cup)
+        return " ".join(map(parens_around_current_cup, str_cups))
 
 def parse_input(text):
     for char in text:
-        yield Cup(char)
+        yield Cup(char, int(char))
 
 def part1(text):
     cups = list(parse_input(text))
-    game = Game(cups)
+    game = Game(cups, 9)
     for _ in range(100):
         game.move()
     return game.order()
 
+def part2(text):
+    cups = list(parse_input(text))
+    cups += list(map(lambda n: Cup(str(n), n), range(10, 1000000+1)))
+    game = Game(cups, 1000000)
+    for _ in range(10000000):
+        game.move()
+    return game.product()
+
 sample = "389125467"
 assert_equals(part1(sample), "67384529")
+assert_equals(part2(sample), 149245887792)
 
 inputtext = "364297581"
 print(part1(inputtext))
+print(part2(inputtext))
