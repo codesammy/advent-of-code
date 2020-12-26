42a231d53dda4c4ee200621e8c5fc08703f2718b
codesammy <codesammy@example.org>
2020-12-26 13:52:22 +0100

    day 4 part 2

--- aoc2019/day04_secure_container.py
+++ aoc2019/day04_secure_container.py
@@ -24,23 +24,52 @@ def compare_adjacent_digits(n, pred):
         yield pred(int(last), int(digit))
         last = digit
 
-def rules(n):
+def same_groups(n):
+    it = iter(str(n))
+    group = [next(it)]
+    for digit in it:
+        last = group[-1]
+        if digit == last:
+            group.append(digit)
+        else:
+            if len(group) > 1:
+                yield group
+            group = [digit]
+    if len(group) > 1:
+        yield group
+
+def rules_part1(n):
     rules = []
     rules.append(same_or_increasing_digits(n))
     rules.append(any(compare_adjacent_digits(n, same)))
     return all(rules)
 
-def part1(text):
-    lower, upper = tuple(map(int, text.split('-')))
-    count = 0
-    for i in range(lower, upper + 1):
-        if rules(i):
-            count += 1
-    return count
+def rules_part2(n):
+    rules = []
+    rules.append(same_or_increasing_digits(n))
+    rules.append(any(len(x) == 2 for x in same_groups(n)))
+    return all(rules)
 
-assert_equals(rules(111111), True)
-assert_equals(rules(223450), False)
-assert_equals(rules(123789), False)
+def parse_input(text):
+    return tuple(map(int, text.split('-')))
+
+def part1(text):
+    lower, upper = parse_input(text)
+    return sum(1 for i in range(lower, upper + 1) if rules_part1(i))
+
+def part2(text):
+    lower, upper = parse_input(text)
+    return sum(1 for i in range(lower, upper + 1) if rules_part2(i))
+
+assert_equals(rules_part1(111111), True)
+assert_equals(rules_part1(223450), False)
+assert_equals(rules_part1(123789), False)
+
+assert_equals(rules_part2(112233), True)
+assert_equals(rules_part2(123444), False)
+assert_equals(rules_part2(111122), True)
+assert_equals(rules_part2(112223), True)
 
 inputtext = "347312-805915"
 print(part1(inputtext))
+print(part2(inputtext))
