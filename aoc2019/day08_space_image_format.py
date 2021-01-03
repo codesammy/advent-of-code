64bf60177599021ccdf7d1c45aee2d3d14312ab1
codesammy <codesammy@example.org>
2021-01-03 14:22:34 +0100

    day 8 part 2

--- aoc2019/day08_space_image_format.py
+++ aoc2019/day08_space_image_format.py
@@ -14,11 +14,16 @@ def part1(text, w, h):
     zeros, product = min(counts, key=itemgetter(0))
     return product
 
+def part2(text, w, h):
+    return "\n".join(["".join(next(c for layer in range(len(text)//(w*h)) if (c:=text[x+y*w+layer*w*h]) != '2') for x in range(w)) for y in range(h)])
+
 class Test(TestCase):
 
     def runTest(self):
         sample = '123456789012'
         self.assertEqual(part1(sample, 3, 2), 1)
+        sample = '0222112222120000'
+        self.assertEqual(part2(sample, 2, 2), '01\n10')
 
 if __name__ == '__main__' or True:
     Test().debug()
@@ -26,3 +31,4 @@ if __name__ == '__main__' or True:
     # find the layer that contains the fewest 0 digits
     # On that layer, what is the number of 1 digits multiplied by the number of 2 digits?
     print(part1(inputtext, 25, 6))
+    print(part2(inputtext, 25, 6))
